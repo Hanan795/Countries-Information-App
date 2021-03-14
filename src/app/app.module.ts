534ee6152037f41deba43cfefca29371e450c1ca
Hanan795 <hawawdeh95@gmail.com>
2021-03-14 09:22:58 +0200

    components setup #4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SelectionsComponent } from './countrySelections/selections.component';
+import { CountriesComponent } from './countries/countries.component';
+import { CountrySelectorsComponent } from './country-selectors/country-selectors.component';
+import { CountryComponent } from './country/country.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SelectionsComponent,
+    CountriesComponent,
+    CountrySelectorsComponent,
+    CountryComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/country-selectors/country-selectors.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-country-selectors',
+  templateUrl: './country-selectors.component.html',
+  styleUrls: ['./country-selectors.component.css']
+})
+export class CountrySelectorsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
