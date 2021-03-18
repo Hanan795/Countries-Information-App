12fd1db58caaae858830a3ad54b2689b814a6e88
Hanan795 <hawawdeh95@gmail.com>
2021-03-18 14:51:25 +0200

    edit app component and app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { SelectionsComponent } from './countrySelections/selections.component';
 import { CountriesComponent } from './countries/countries.component';
 import { CountrySelectorsComponent } from './country-selectors/country-selectors.component';
 import { CountryComponent } from './country/country.component';
+import { RouterModule } from '@angular/router'
+import { routingTable } from './routes';
+import { RegionCountriesComponent } from './region-countries/region-countries.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    SelectionsComponent,
     CountriesComponent,
     CountrySelectorsComponent,
-    CountryComponent
+    CountryComponent,
+    RegionCountriesComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule, RouterModule.forRoot(routingTable)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
